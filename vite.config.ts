20ac7aef93029c044ce73ca51940d55a9f9a2176
Tim Lebrun <tim.lebrun@airweb.fr>
2025-02-18 14:23:19 +0100

    :art: Cleaning up configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,8 @@
 import { defineConfig } from 'vite';
-import { join } from 'path';
 
 import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
 	root: './',
-	server: {
-		open: join(__dirname, 'public', 'index.html'),
-	},
-	build: {
-		rollupOptions: {
-			input: join(__dirname, 'public', 'index.html'),
-		},
-	},
 	plugins: [basicSsl()],
 });
